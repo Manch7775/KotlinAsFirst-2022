283aeb495bf169f46a55d7cab4055692006b7d83
Manch7775 <112945783+Manch7775@users.noreply.github.com>
2022-11-09 21:19:13 +0300

    fh

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -201,12 +201,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean {
-    val set = word.toSet()
-    return set.all {
-        it in chars
-    }
-}
+fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
 
 /**
  * Средняя (4 балла)
