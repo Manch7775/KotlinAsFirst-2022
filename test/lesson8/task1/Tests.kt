a94e58d76c1f622deb1e6aff232e52831377d9fb
Manch7775 <112945783+Manch7775@users.noreply.github.com>
2022-12-13 23:40:27 +0300

    dg

--- test/lesson8/task1/Tests.kt
+++ test/lesson8/task1/Tests.kt
@@ -217,8 +217,6 @@ class Tests {
     fun bisectorByPoints() {
         assertApproxEquals(Line(Point(2.0, 0.0), PI / 2), bisectorByPoints(Point(0.0, 0.0), Point(4.0, 0.0)))
         assertApproxEquals(Line(Point(1.0, 2.0), 0.0), bisectorByPoints(Point(1.0, 5.0), Point(1.0, -1.0)))
-        assertApproxEquals(Line(Point( 1.0, 2.0), 0.0), bisectorByPoints(Point(   -2.220446049250313e-16,
-             0.10764255687642377), Point(0.0, 0.9669054875566967)))
     }
 
     @Test
